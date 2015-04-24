2e76a6fe6a89f5ac8171439e4470babc716edf30
王凡 <hbh2010@126.com>
2015-04-24 17:00:47 +0800

    BP structed
    BP structed
    2015-04-24 by wf

---
+++ FI/Code/FIBP/BpImplement/OperatorBonusBP/BonusRecordDTOExtend.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace UFIDA.U9.Cust.GS.FI.OperatorBonusBP {
+
+	public partial class BonusRecordDTO{
+
+		#region Constructor
+		/// <summary>
+		/// Constructor with Full Argument 
+		/// </summary>
+		public BonusRecordDTO(  List<System.Int32> listYear  , List<UFIDA.U9.Cust.GS.FI.BonusCalculateRecordBE.BonusCalculateRecordDTO> bonusCalculateRecord  )
+		{
+			this.ListYear = listYear;
+			this.BonusCalculateRecord = bonusCalculateRecord;
+		}
+		#endregion	
+
+
+
+
+
+
+		#region Model Methods
+		#endregion
+
+	}
+}
