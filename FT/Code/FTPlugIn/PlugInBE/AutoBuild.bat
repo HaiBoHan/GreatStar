1e7bf12cf0950ac6cd337fd0d3220915ae23651f
王凡 <hbh2010@126.com>
2015-04-24 16:29:35 +0800

    Omit files add
    2015-04-24 by wf
    
    1、E:\U9客开\UFIDA\U9Proj\GreatStar\FI\Code\PaymentUI\Model\UFIDA.U9.Cust.GS.FI.PrePaymentUI.Model.csproj
    
    */Code/*/Model/*
    !*/Code/*/Model/*Extend.cs
    #YuNeng have ,so have too .
    !*/Code/*/Model/*.Model.csproj
    
    2、公共方法，针对 .csproj , .sln 同级目录的 .cs文件；
    
    3、
    !FT/Code/FTPlugIn/FTPlugIn.sln
    !FT/Code/FTPlugIn/PlugInUI/Properties/AssemblyInfo.cs
    !FT/Code/FTPlugIn/PlugInBE/Properties/AssemblyInfo.cs
    !FT/Code/FTPlugIn/PlugInBE/AutoBuild.bat
    !FT/Code/FTPlugIn/PlugInUI/AutoBuild.bat
    
    只能改成这样，不明；
    !FT/Code/FTPlugIn/PlugInUI/Properties*
    !FT/Code/FTPlugIn/PlugInBE/Properties*

---
+++ FT/Code/FTPlugIn/PlugInBE/AutoBuild.bat
@@ -0,0 +1,5 @@
+@echo off
+copy .\Bin\Debug\UFIDA.U9.Cust.GS.FT.PlugInBE.dll d:\ufida\u9v30\Portal\ApplicationServer\Libs
+copy .\Bin\Debug\UFIDA.U9.Cust.GS.FT.PlugInBE.pdb d:\ufida\u9v30\Portal\ApplicationServer\Libs
+
+pause
---
+++ FT/Code/FTPlugIn/PlugInUI/AutoBuild.bat
@@ -0,0 +1,5 @@
+@echo off
+copy .\Bin\Debug\UFIDA.U9.Cust.GS.FT.PlugInUI.dll d:\ufida\u9v30\portal\uilib
+copy .\Bin\Debug\UFIDA.U9.Cust.GS.FT.PlugInUI.pdb d:\ufida\u9v30\portal\uilib
+
+pause
