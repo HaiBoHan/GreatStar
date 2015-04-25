605711071b34ff2fa9a8c85f6d5284ce24939e75
王凡 <hbh2010@126.com>
2015-04-25 09:40:21 +0800

    BP structed
    BP structed
    2015-04-25 by wf

---
+++ FT/Code/FTBP/BpImplement/OperateShipPlanBP/SupplierOrItemDTOExtend.cs
@@ -0,0 +1,68 @@
+﻿
+
+
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace UFIDA.U9.Cust.GS.FT.OperateShipPlanBP {
+
+	public partial class SupplierOrItemDTO{
+
+		#region Constructor
+		/// <summary>
+		/// Constructor with Full Argument 
+		/// </summary>
+		public SupplierOrItemDTO(  System.Int64 shipItemFee  , System.String shipItemFee_Code  , System.String shipItemFee_Name  , System.Int64 shipFeeProject  , System.String shipFeeProject_Code  , System.String shipFeeProject_Name  , System.Int64 combination  , System.String combination_Code  , System.String combination_Name  , System.Decimal taxRate  , System.Int64 uom  , System.String uom_Code  , System.String uom_Name  , System.Int32 allocationWay  , System.Int64 supplier  , System.String supplier_Code  , System.String supplier_Name  , System.Int64 item  , System.String item_Code  , System.String item_Name  , System.String symbol  , System.Int32 moneyRound_Precision  , System.Int32 moneyRound_RoundType  , System.Int32 moneyRound_RoundValue  , System.Int32 priceRound_Precision  , System.Int32 priceRound_RoundType  , System.Int32 priceRound_RoundValue  , System.Int64 currency  , System.String currency_Code  , System.String currency_Name  , System.Int32 uom_Round_Precision  , System.Int32 uom_Round_RoundType  , System.Int32 uom_Round_RoundValue  )
+		{
+			this.ShipItemFee = shipItemFee;
+			this.ShipItemFee_Code = shipItemFee_Code;
+			this.ShipItemFee_Name = shipItemFee_Name;
+			this.ShipFeeProject = shipFeeProject;
+			this.ShipFeeProject_Code = shipFeeProject_Code;
+			this.ShipFeeProject_Name = shipFeeProject_Name;
+			this.Combination = combination;
+			this.Combination_Code = combination_Code;
+			this.Combination_Name = combination_Name;
+			this.TaxRate = taxRate;
+			this.Uom = uom;
+			this.Uom_Code = uom_Code;
+			this.Uom_Name = uom_Name;
+			this.AllocationWay = allocationWay;
+			this.Supplier = supplier;
+			this.Supplier_Code = supplier_Code;
+			this.Supplier_Name = supplier_Name;
+			this.Item = item;
+			this.Item_Code = item_Code;
+			this.Item_Name = item_Name;
+			this.Symbol = symbol;
+			this.MoneyRound_Precision = moneyRound_Precision;
+			this.MoneyRound_RoundType = moneyRound_RoundType;
+			this.MoneyRound_RoundValue = moneyRound_RoundValue;
+			this.PriceRound_Precision = priceRound_Precision;
+			this.PriceRound_RoundType = priceRound_RoundType;
+			this.PriceRound_RoundValue = priceRound_RoundValue;
+			this.Currency = currency;
+			this.Currency_Code = currency_Code;
+			this.Currency_Name = currency_Name;
+			this.Uom_Round_Precision = uom_Round_Precision;
+			this.Uom_Round_RoundType = uom_Round_RoundType;
+			this.Uom_Round_RoundValue = uom_Round_RoundValue;
+		}
+		#endregion	
+
+
+
+
+
+
+		#region Model Methods
+		#endregion
+
+	}
+}
